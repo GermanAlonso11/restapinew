3159af00ee25477374ade1ee8b6edebdbed935b9
German Alonso <german.aguiniga11@outlook.es>
2025-08-13 23:27:49 -0700

    corregido

---
+++ apirest-react/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Header from './componentes/Header.jsx';
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <Header />
+  )
+}
+
+export default App;
---
+++ apirest-react/src/componentes/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="barra">
+        <div className="contenedor">
+            <h1>CRM - Admin de Clientes</h1>
+            <h2>hola</h2>
+        </div>
+    </header>
+  );
+}
+
+export default Header;
